38899c4369ce5e80b3b8565b18064bb392d97bdd
Joey <yty0606@gmail.com>
2021-10-01 11:15:39 +0800

    Feat: Build viewModel and repository
    Solution:
    1. As title.
    2. Reference class 74.
    
    Author: Joey

--- MealsAp/app/src/main/java/com/example/mealsap/ui/meals/MainActivity.kt
+++ MealsAp/app/src/main/java/com/example/mealsap/ui/meals/MainActivity.kt
@@ -1,38 +1,42 @@
-package com.example.mealsap
+package com.example.mealsap.ui.meals
+
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.activity.viewModels
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mealsap.ui.theme.MealsApTheme
 
+/**
+ * Created by Joey 2021/10/01
+ * (重要)
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //val viewModel by viewModels<MealsCategoriesViewModel>() // 雖沒用,但(重要) by有委託的意思 , ch74 13:00左右
         setContent {
             MealsApTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MealsCategoriesScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MealsCategoriesScreen() {
+    val viewModel : MealsCategoriesViewModel = viewModel() //(Ch74, 16:00有說明,簡單來說只要存在過,就不會再instantiated一次)
+    Text(text = "Hello Compose!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MealsApTheme {
-        Greeting("Android")
+        MealsCategoriesScreen()
     }
 }
---
+++ MealsAp/app/src/main/java/com/example/mealsap/ui/meals/MealsCategoriesViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.mealsap.ui.meals
+
+import androidx.lifecycle.ViewModel
+import com.example.model.MealsRepository
+
+class MealsCategoriesViewModel (private val repository: MealsRepository = MealsRepository()): ViewModel() {
+    // inherit ViewModel()
+}
